acfe7b7c4fb2d9cd8e32607af56a67951f8c7480
Prashant Mittal <pro.mit3863@gmail.com>
2023-12-07 13:15:42 +0530

    fixing types in adaptor

--- helia_wnfs_blockstore_adaptor.js
+++ helia_wnfs_blockstore_adaptor.js
@@ -7,16 +7,15 @@ export class WnfsBlockstore {
   }
 
   async getBlock (cid){
-    return this.helia.blockstore.get(cid)
+    const decoded_cid = CID.decode(cid)
+    return this.helia.blockstore.get(decoded_cid)
   }
   
   async putBlock (bytes, codec){
-    console.log("I am here")
     const multihash = await sha256.digest(bytes)
     const cid = CID.createV1(codec, multihash)
     this.helia.blockstore.put(cid, bytes)
-    console.log("bye")
-    return cid
+    return cid.bytes
   }
 }
 
