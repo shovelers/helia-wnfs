8434f14a87fd0c7d1d9b633287da687c96de0894
Prashant Mittal <pro.mit3863@gmail.com>
2023-12-16 19:43:01 +0530

    Refactoring the codebase

---
+++ src/browser.js
@@ -0,0 +1,6 @@
+import { createBrowserNode, dial } from './helia_node.js';
+import { ExamplePublicFile } from './example_public_file.js';
+import { ExamplePrivateFile } from './example_private_file.js';
+import { Recipient } from './share.js';
+
+export { dial, createBrowserNode, ExamplePublicFile, ExamplePrivateFile, Recipient }
